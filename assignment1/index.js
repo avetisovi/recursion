0d2493ff8a5acbe5e90d2fdfcc6bf40736691eaf
Anatoli Avetisovi <tolikatolika794@gmail.com>
2022-09-15 16:06:12 +0400

    added recursive solution for fibonacci function

--- assignment1/index.js
+++ assignment1/index.js
@@ -2,10 +2,17 @@ function fibs(num) {
   let total = [];
   for (let i = 0; i < num; i++) {
     if (i <= 1) {
-      total.push(i)
+      total.push(i);
     } else {
-      total.push(total[i - 1] + total[i - 2])
+      total.push(total[i - 1] + total[i - 2]);
     }
   }
-  return total
+  return total;
+}
+
+function fibsRec(num) {
+  if (num <= 1) return [0];
+
+  const prev = fibsRec(num - 1);
+  return [...prev, prev.at(-1) + prev.at(-2) || 1];
 }
